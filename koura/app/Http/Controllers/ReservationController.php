51b5473f59388ea850f5fe366f3199ef33b248f2
Emrane23 <88634484+Emrane23@users.noreply.github.com>
2022-01-19 00:00:13 +0100

    Update ReservationController.php

--- koura/app/Http/Controllers/ReservationController.php
+++ koura/app/Http/Controllers/ReservationController.php
@@ -166,7 +166,7 @@ class ReservationController extends Controller
     //   DB::raw('count(*) as nbr_reservation'),  DB::raw('DATE(date) as date')
     // ])->groupBy('date')
     // ->get();
-    $liste= DB::table('reservations')->select(DB::raw('DATE(date) as date'), DB::raw('count(*) as nbr_reservation'))
+    $liste= DB::table('reservations')->select(DB::raw('DATE(date) as date'), DB::raw('count(*) as title'))
         ->groupBy('date')
         ->get();
         return response()->json($liste, 200); 
